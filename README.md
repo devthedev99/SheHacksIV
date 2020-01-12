848021907a22973b05be46a3a86cffd832babf1c
devthedev99 <dnanan2@uwo.ca>
2020-01-11 19:13:01 -0500

    added logout and login functionalities

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SheHacksIV
-This purpose of this website is to be used by doctors to enter their patient information and/or upload an image, and, using Google's Vision API, it will detec(h)t whether or not the patient's cancer is malignant or benign.
+This main purpose of this website is to be used by doctors to enter their patient information and/or upload an image, and, using Google's Vision API, it will detec(h)t whether or not the patient's cancer is malignant or benign. 
+
+Patients can also use the website for themselves to check up on their own health. 
+
+Users must login because medical information is private. 
--- scripts/auth.js
+++ scripts/auth.js
@@ -7,7 +7,8 @@ signupForm.addEventListener('submit', (e) => {
   const email = signupForm['signup-email'].value;
   const password = signupForm['signup-password'].value;
 
-  // sign up the user
+  // sign up the user 
+  // signup automatically logs in
   auth.createUserWithEmailAndPassword(email, password).then(cred => {
     console.log(cred.user);
     // close the signup modal & reset form
@@ -15,4 +16,33 @@ signupForm.addEventListener('submit', (e) => {
     M.Modal.getInstance(modal).close();
     signupForm.reset();
   });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+  })
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+
 });
